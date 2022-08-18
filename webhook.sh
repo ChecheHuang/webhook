ae56aa925eea65316f06eefe0f8c744c5a20f18e
Carl <carlhuang@sunnygo.com.tw>
2022-08-18 14:15:49 +0800

    modify

--- webhook.sh
+++ webhook.sh
@@ -31,7 +31,7 @@ if [ -d "$gitPath" ]; then
         fi
         #拉取項目最新文件
         #git reset --hard origin/master
-        git pull && cd react && npm run build && cp -rp /home/ubuntu/webhook/react/build /home/ubuntu/webhook
+        # git pull && cd react && npm run build && cp -rp /home/ubuntu/webhook/react/build /home/ubuntu/webhook
         
         echo "我要結束了"
         exit
@@ -40,4 +40,5 @@ else
         echo "End"
         exit
 fi
+echo "腳本結束"
 
